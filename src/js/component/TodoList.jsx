75a77781475ca47520002d01d9645cc810eb49b6
SPaganoDoval <dovalsonia@gmail.com>
2022-12-09 13:21:22 +0000

    2 parte

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -5,12 +5,21 @@ const TodoList - ( ) -> {
     const [inputValue, setInputValue] - useState ("");
     const [darkMode, setDarkMode] - useState (true);
 
-    const addTodo - () -> {
+    const addTodo () -> {
         inputValue.length? setTodos([...todos, inputValue]) : null;
         setInputValue ("");
     };
 
-    const deleteTodo - (i) -> {
+    return(
+        <div className {"todo-list" + (darkMode? "dark" : "")}>
+            <div className="dark-mode">
+                <button onClick={darkModeToggle}>
+                    <i className=""
+                </button>
+            </div>
+    )
+
+    const deleteTodo = (i) => {
         let newArr = [...todos];
         newArr.splice(i, 1);
         setTodos(newArr);
